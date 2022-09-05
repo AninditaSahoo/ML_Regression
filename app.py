e9150ed1949b9a0a868edbdc0096b886a0e4b9f3
AninditaSahoo <analyticsanindita@gmail.com>
2022-09-05 11:18:28 +0530

    logging and exception module

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import sys
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
 
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
-def index():
+def index():    
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing log module")
     return "Starting Machine Learning Project and observed the CI/CD pipeline"
 
 if __name__=="__main__":
